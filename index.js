9cf12d5c5cf3d0ec9585b0f58b90536d0e36ccb2
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-06-03 17:04:49 -0400

    no more ecdh deduction, only identity/addressbook based loading

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 var Q = require('q')
-var bitcoin = require('bitcoinjs-lib')
 var typeForce = require('typeforce')
 var utils = require('tradle-utils')
 var debug = require('debug')('chainloader')
 var find = require('array-find')
-// var bitcoin = require('bitcoinjs-lib')
 var EventEmitter = require('events').EventEmitter
 var inherits = require('util').inherits
 var pluck = require('array-pluck')
@@ -36,7 +34,7 @@ function Loader (options) {
 
   FILE_EVENTS.forEach(function (event) {
     self.on(event, function (data) {
-      self.saveIfNew(data)
+      // self.saveIfNew(data)
       self.emit('file', data)
     })
   })
@@ -61,12 +59,12 @@ Loader.prototype.load = function (txs) {
   var shared
   var files = []
   return this.fetchFiles(keys)
-    .then(function (files) {
-      if (!files.length) return
+    .then(function (fetched) {
+      if (!fetched.length) return
 
       pub.forEach(function (parsed, i) {
-        if (files[i]) {
-          parsed.file = files[i]
+        if (fetched[i]) {
+          parsed.file = fetched[i]
           self.emit('file:public', parsed)
           files.push(parsed)
         }
@@ -75,7 +73,7 @@ Loader.prototype.load = function (txs) {
       if (!enc.length) return
 
       shared = enc.filter(function (parsed, i) {
-        var file = files[i + pub.length]
+        var file = fetched[i + pub.length]
         if (!file) return
 
         try {
@@ -94,110 +92,109 @@ Loader.prototype.load = function (txs) {
       return self.fetchFiles(pluck(shared, 'key'))
     })
     .then(function (sharedFiles) {
-      if (!sharedFiles) return
+      if (sharedFiles) {
+        sharedFiles.forEach(function (file, idx) {
+          var parsed = extend({}, shared[idx])
+          var pKey = parsed.key
+          parsed.key = parsed.permission.fileKeyString()
+          parsed.permissionKey = pKey
+          parsed.type = 'sharedfile'
 
-      sharedFiles.forEach(function (file, idx) {
-        var parsed = extend({}, shared[idx])
-        var pKey = parsed.key
-        parsed.key = parsed.permission.fileKeyString()
-        parsed.permissionKey = pKey
-        parsed.type = 'sharedfile'
-
-        var decryptionKey = parsed.permission.decryptionKeyBuf()
-        if (decryptionKey) {
-          try {
-            file = utils.decrypt(file, decryptionKey)
-          } catch (err) {
-            debug('Failed to decrypt ciphertext: ' + file)
-            return
+          var decryptionKey = parsed.permission.decryptionKeyBuf()
+          if (decryptionKey) {
+            try {
+              file = utils.decrypt(file, decryptionKey)
+            } catch (err) {
+              debug('Failed to decrypt ciphertext: ' + file)
+              return
+            }
           }
-        }
 
-        parsed.file = file
-        self.emit('file:shared', parsed)
-        files.push(parsed)
+          parsed.file = file
+          self.emit('file:shared', parsed)
+          files.push(parsed)
+        })
+      }
+
+      return files.sort(function (a, b) {
+        return txs.indexOf(a.tx.body) - txs.indexOf(b.tx.body)
       })
-
-      return files
     })
 }
 
-/**
- * Attempt to deduce the permission key and ECDH shared key
- *   from the parties involved in the bitcoin transaction
- * @param  {Transaction} tx
- * @param  {TransactionData} txData
- * @return {Object}   permission file "key" and ECDH "sharedKey" to decrypt it
- */
-Loader.prototype.deduceECDHKeys = function (tx, txData) {
-  if (!(this.wallet && txData)) return
+// /**
+//  * Attempt to deduce the permission key and ECDH shared key
+//  *   from the parties involved in the bitcoin transaction
+//  * @param  {Transaction} tx
+//  * @param  {TransactionData} txData
+//  * @return {Object}   permission file "key" and ECDH "sharedKey" to decrypt it
+//  */
+// Loader.prototype.deduceECDHKeys = function (tx, txData) {
+//   if (!(this.wallet && txData)) return
 
-  var wallet = this.wallet
-  var myAddress
-  var myPrivKey
-  var theirPubKey
-  var toMe = this.getSentToMe(tx)
-  var fromMe = this.getSentFromMe(tx)
-  if (!toMe.length && !fromMe.length) {
-    debug("Cannot parse permission data from transaction as it's neither to me nor from me")
-    return
-  }
+//   var wallet = this.wallet
+//   var myAddress
+//   var myPrivKey
+//   var theirPubKey
+//   var toMe = this.getSentToMe(tx)
+//   var fromMe = this.getSentFromMe(tx)
+//   if (!toMe.length && !fromMe.length) {
+//     debug("Cannot parse permission data from transaction as it's neither to me nor from me")
+//     return
+//   }
 
-  if (fromMe.length) {
-    // can't figure out their public key
-    if (toMe.length !== tx.outs.length - 1) {
-      debug("Unable to process transaction data, don't know the public key of the receipient")
-      return
-    }
+//   if (fromMe.length) {
+//     tx.ins.some(function (input) {
+//       var addr = utils.getAddressFromInput(input, this.networkName)
+//       myPrivKey = wallet.addressString === addr && wallet.priv
+//       return myPrivKey
+//     }, this)
 
-    tx.ins.some(function (input) {
-      var addr = utils.getAddressFromInput(input, this.networkName)
-      myPrivKey = wallet.getPrivateKeyForAddress(addr)
-      return myPrivKey
-    }, this)
+//     toMe.some(function (out) {
+//       var addr = utils.getAddressFromOutput(out, this.networkName)
+//       theirPubKey = addr === wallet.addressString && wallet.pub
+//       return theirPubKey
+//     }, this)
+//   } else {
+//     myAddress = utils.getAddressFromOutput(toMe[0], this.networkName)
+//     myPrivKey = wallet.addressString === myAddress && wallet.priv
+//     theirPubKey = bitcoin.ECPubKey.fromBuffer(tx.ins[0].script.chunks[1])
+//   }
 
-    toMe.some(function (out) {
-      var addr = utils.getAddressFromOutput(out, this.networkName)
-      if (addr && !wallet.isChangeAddress(addr)) {
-        theirPubKey = wallet.getPublicKeyForAddress(addr)
-        return true
-      }
-    }, this)
-  } else {
-    myAddress = utils.getAddressFromOutput(toMe[0], this.networkName)
-    myPrivKey = wallet.getPrivateKeyForAddress(myAddress)
-    theirPubKey = bitcoin.ECPubKey.fromBuffer(tx.ins[0].script.chunks[1])
-  }
+//   if (myPrivKey && theirPubKey) {
+//     if (myPrivKey.pub.toHex() !== theirPubKey.toHex()) {
+//       return {
+//         priv: myPrivKey,
+//         pub: theirPubKey
+//       }
+//     }
+//   }
 
-  return myPrivKey && theirPubKey && {
-    priv: myPrivKey,
-    pub: theirPubKey
-  }
-}
+// }
 
 /**
  *  @return {Array} outputs in tx that the underlying wallet can spend
  */
-Loader.prototype.getSentToMe = function (tx) {
-  if (!this.wallet) return []
+// Loader.prototype.getSentToMe = function (tx) {
+//   if (!this.wallet) return []
 
-  return tx.outs.filter(function (out) {
-    var address = utils.getAddressFromOutput(out, this.networkName)
-    return this.wallet.getPrivateKeyForAddress(address) && out
-  }, this)
-}
+//   return tx.outs.filter(function (out) {
+//     var address = utils.getAddressFromOutput(out, this.networkName)
+//     return this.wallet.addressString === address
+//   }, this)
+// }
 
 /**
  *  @return {Array} inputs in tx that are signed by the underlying wallet
  */
-Loader.prototype.getSentFromMe = function (tx) {
-  if (!this.wallet) return []
+// Loader.prototype.getSentFromMe = function (tx) {
+//   if (!this.wallet) return []
 
-  return tx.ins.filter(function (input) {
-    var address = utils.getAddressFromInput(input, this.networkName)
-    return this.wallet.getPrivateKeyForAddress(address) && input
-  }, this)
-}
+//   return tx.ins.filter(function (input) {
+//     var address = utils.getAddressFromInput(input, this.networkName)
+//     return this.wallet.addressString === address
+//   }, this)
+// }
 
 Loader.prototype.fetchFiles = function (keys) {
   return this.keeper.getMany(keys)
@@ -207,23 +204,23 @@ Loader.prototype.fetchFiles = function (keys) {
     })
 }
 
-Loader.prototype.saveIfNew = function (data) {
-  var self = this
+// Loader.prototype.saveIfNew = function (data) {
+//   var self = this
 
-  var wallet = this.wallet
-  if (!wallet) return
+//   var wallet = this.wallet
+//   if (!wallet) return
 
-  var tx = data.tx.body
-  var metadata = data.tx.metadata
-  if (!metadata || metadata.confirmations) return
+//   var tx = data.tx.body
+//   var metadata = data.tx.metadata
+//   if (!metadata || metadata.confirmations) return
 
-  var received = !wallet.isSentByMe(tx)
-  var type = received ? 'received' : 'sent'
-  return this.keeper.put(data.file)
-    .then(function () {
-      self.emit('file:' + type, data)
-    })
-}
+//   var received = !wallet.isSentByMe(tx)
+//   var type = received ? 'received' : 'sent'
+//   return this.keeper.put(data.file)
+//     .then(function () {
+//       self.emit('file:' + type, data)
+//     })
+// }
 
 Loader.prototype._getSharedKey = function (parsed) {
   var me = this.identity
@@ -235,21 +232,21 @@ Loader.prototype._getSharedKey = function (parsed) {
     if (me === from.identity || me === to.identity) {
       priv = me === from.identity ? from.key.priv() : to.key.priv()
       pub = me === from.identity ? to.key.pub() : from.key.pub()
+      return priv && pub && utils.sharedEncryptionKey(priv, pub)
     }
   }
 
-  if (!(priv && pub)) {
-    // priv = ...
-    // pub = ...
-    // TODO: fall back to decrypting based on bitcoin keys associated with this tx
-    var keys = this.deduceECDHKeys(parsed.tx.body, parsed.key)
-    if (!keys) return
+  // if (!(priv && pub)) {
+  //   // priv = ...
+  //   // pub = ...
+  //   // TODO: fall back to decrypting based on bitcoin keys associated with this tx
+  //   var keys = this.deduceECDHKeys(parsed.tx.body, parsed.key)
+  //   if (!keys) return
 
-    pub = keys.pub
-    priv = keys.priv
-  }
+  //   pub = keys.pub
+  //   priv = keys.priv
+  // }
 
-  return utils.sharedEncryptionKey(priv, pub)
 }
 
 Loader.prototype._parseTxs = function (txs) {
@@ -261,22 +258,49 @@ Loader.prototype._parseTxs = function (txs) {
 
     var from
     var to
-    if (self.addressBook) {
-      var addrs = parsed.tx.addresses
+    var addrs = parsed.tx.addresses
+    if (self.identity) {
       find(addrs.from, function (addr) {
-        from = self.addressBook.byFingerprint(addr)
+        var key = self.identity.keys({ fingerprint: addr })[0]
+        from = key && {
+          key: key,
+          identity: self.identity
+        }
+
         return from
       })
 
-      find(addrs.to, function (addr) {
-        to = self.addressBook.byFingerprint(addr)
-        return to
-      })
+      if (from.identity !== self.identity) {
+        find(addrs.to, function (addr) {
+          var key = self.identity.keys({ fingerprint: addr })[0]
+          to = key && {
+            key: key,
+            identity: self.identity
+          }
 
-      parsed.from = from
-      parsed.to = to
+          return to
+        })
+      }
     }
 
+    if (self.addressBook) {
+      if (!from) {
+        find(addrs.from, function (addr) {
+          from = self.addressBook.byFingerprint(addr)
+          return from
+        })
+      }
+
+      if (!to) {
+        find(addrs.to, function (addr) {
+          to = self.addressBook.byFingerprint(addr)
+          return to
+        })
+      }
+    }
+
+    parsed.from = from
+    parsed.to = to
     if (parsed.type !== 'public') {
       parsed.sharedKey = self._getSharedKey(parsed)
       if (parsed.sharedKey) {
